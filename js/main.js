b6394a56f3e6e86c5146d3cc886c78382b9b8a55
marko18399 <marko.savicevic52@gmail.com>
2020-12-20 22:23:24 +0100

    Dodat alert na katalog i kontakt

---
+++ js/main.js
@@ -0,0 +1,8 @@
+if(window.location.href.indexOf('katalog.html')!= -1){
+    alert("Marko Savićević");
+}
+
+if(window.location.href.indexOf('kontakt.html')!= -1){
+    alert("Da li želite da nastavite?");
+    }
+    
